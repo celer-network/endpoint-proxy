298a7bf7808532ecbd426061855c0fed5285f2a2
liuxiaobleach <1241368737@qq.com>
2022-04-20 15:53:07 +0800

    support plat on

---
+++ endpointproxy/platonproxy.go
@@ -0,0 +1,63 @@
+package endpointproxy
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+
+	"github.com/celer-network/goutils/log"
+)
+
+type PlatonProxy struct {
+	platonTargetUrl *url.URL
+}
+
+// NewProxy takes target host and creates a reverse proxy
+func (h *PlatonProxy) startPlatonProxy(targetHost string, port int) error {
+	var err error
+	h.platonTargetUrl, err = url.Parse(targetHost)
+	if err != nil {
+		return err
+	}
+	p := httputil.NewSingleHostReverseProxy(h.platonTargetUrl)
+	originalDirector := p.Director
+	p.Director = func(req *http.Request) {
+		originalDirector(req)
+		h.modifyPlatonRequest(req)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", proxyRequestHandler(p))
+	go startCustomProxyByPort(port, mux)
+	return nil
+}
+
+func (h *PlatonProxy) modifyPlatonRequest(req *http.Request) {
+	req.URL.Scheme = h.platonTargetUrl.Scheme
+	req.URL.Host = h.platonTargetUrl.Host
+	req.Host = h.platonTargetUrl.Host
+	reqStr, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Errorf("invalid platon request err:%s", err.Error())
+		return
+	}
+	msg := &jsonrpcMessage{}
+	if err = json.Unmarshal(reqStr, msg); err != nil {
+		log.Errorf("fail to unmarshal this platon req body err:%s", err.Error())
+		return
+	}
+	if msg.Method == MethodEthGetCode {
+		newParams := strings.Replace(string(msg.Params), "\"pending\"", "\"latest\"", 1)
+		msg.Params = []byte(newParams)
+	}
+	newMsg, marshalErr := json.Marshal(msg)
+	if marshalErr != nil {
+		log.Errorf("fail to marshal this new platon req, raw:%s, err:%s", string(newMsg), marshalErr.Error())
+		return
+	}
+	req.Body = ioutil.NopCloser(bytes.NewReader(newMsg))
+	req.ContentLength = int64(len(newMsg))
+}
